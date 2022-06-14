eb0da58b6dacb51ccd02e2792133a678d65edb8a
snappixd <levinandrey33@gmail.com>
2022-06-14 22:45:58 +0100

    added photo base64 to db

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,7 +18,7 @@ type Server struct {
 func NewServer(handler http.Handler) *Server {
 	return &Server{
 		httpServer: &http.Server{
-			Addr:           ":" + "80", //os.Getenv("PORT"),
+			Addr:           ":" + os.Getenv("PORT"),
 			Handler:        handler,
 			ReadTimeout:    RWTimeout,
 			WriteTimeout:   RWTimeout,
